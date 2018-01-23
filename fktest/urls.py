a4941d9f269d1d9310f2c496aa5a8ae6b0bcb1e8
liruisee <lirui_gydx@sina.com>
2018-01-23 16:09:53 +0800

    项目上传

---
+++ fktest/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from myapp import urls
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'fktest.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'myapp/', include(urls))
+]
