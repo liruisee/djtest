20c3e9880b3aa459e206a02c812fd634162c0809
liruisee <lirui_gydx@sina.com>
2018-02-26 17:56:02 +0800

    jijml

--- fktest/settings.py
+++ fktest/settings.py
@@ -103,9 +103,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ([os.path.join(BASE_DIR, 'static')])
 # STATIC_PATH = os.path.join(BASE_DIR, 'static/').replace('\\', '/')
